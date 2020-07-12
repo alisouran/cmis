da729ed3b56c81e4e1dca3081c93c5920d0d571b
alisouran <alirezasoran@gmail.com>
2020-07-12 04:46:24 +0430

    first

---
+++ precache-manifest.2058825e02662da66b398140caaec2d6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f695b11dc7759953351dd5c13fede80b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb6a5c940969c30fb88",
+    "url": "/static/css/main.ce87affc.chunk.css"
+  },
+  {
+    "revision": "450c9145c374828b1acd",
+    "url": "/static/js/2.93f9250b.chunk.js"
+  },
+  {
+    "revision": "cd8ad9ee0daa26199631ac4b895a4e7e",
+    "url": "/static/js/2.93f9250b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb6a5c940969c30fb88",
+    "url": "/static/js/main.be200ba6.chunk.js"
+  },
+  {
+    "revision": "44dbebda7b28db8cfcc3",
+    "url": "/static/js/runtime-main.0348ee6a.js"
+  },
+  {
+    "revision": "3bde74f636a8ca0703e34854a662f29b",
+    "url": "/static/media/1.3bde74f6.jpg"
+  },
+  {
+    "revision": "fb92d9edbf9c9f9b2953617b71e8aae7",
+    "url": "/static/media/1.fb92d9ed.JPG"
+  },
+  {
+    "revision": "128c55db0ef08df50ae5a3d2bfbece1e",
+    "url": "/static/media/12.128c55db.JPG"
+  },
+  {
+    "revision": "5ebf60ef0b06a54ea694cf7d09b7fb72",
+    "url": "/static/media/2.5ebf60ef.jpg"
+  },
+  {
+    "revision": "59911e34bf00ed718c00041205f5e212",
+    "url": "/static/media/3.59911e34.jpg"
+  },
+  {
+    "revision": "dda9ccb4e7a4ef613bbe46b7a1db2d03",
+    "url": "/static/media/4.dda9ccb4.jpg"
+  },
+  {
+    "revision": "30afeeb945e15ed96fde92d70ba55f0e",
+    "url": "/static/media/ISF_BrandAssets_ChapterLogo BW.30afeeb9.png"
+  },
+  {
+    "revision": "0c599114ed9ecfb118f14c12b797a862",
+    "url": "/static/media/Mailchimp.0c599114.png"
+  },
+  {
+    "revision": "32a9365c444760242e313a6c0ee5e76a",
+    "url": "/static/media/Vazir-Bold.32a9365c.woff"
+  },
+  {
+    "revision": "568d89450f813dfc78bd1d0fa948f573",
+    "url": "/static/media/Vazir-Bold.568d8945.eot"
+  },
+  {
+    "revision": "792a144ff1ad9229e395f84859f84bad",
+    "url": "/static/media/Vazir-Bold.792a144f.woff2"
+  },
+  {
+    "revision": "e1fa9df04f68b6c11ba73b2cb2c32111",
+    "url": "/static/media/Vazir-Bold.e1fa9df0.ttf"
+  },
+  {
+    "revision": "63dc8aed0230d5693186695dff5fa598",
+    "url": "/static/media/Vazir.63dc8aed.ttf"
+  },
+  {
+    "revision": "78fb045cc70b332ad0eca7f78031acf3",
+    "url": "/static/media/Vazir.78fb045c.woff"
+  },
+  {
+    "revision": "c97b654993f7c62053b0d2b6505b60f2",
+    "url": "/static/media/Vazir.c97b6549.woff2"
+  },
+  {
+    "revision": "e5b0de94c692df249d1903e3f4412289",
+    "url": "/static/media/Vazir.e5b0de94.eot"
+  },
+  {
+    "revision": "40a447d409941e933143654eca04e736",
+    "url": "/static/media/aparat.40a447d4.svg"
+  },
+  {
+    "revision": "26c7f2385fd3da1112637059a2f3d53a",
+    "url": "/static/media/basecamp.26c7f238.png"
+  },
+  {
+    "revision": "073b11104ca4cc5709134b4669561704",
+    "url": "/static/media/creativemornings.073b1110.png"
+  },
+  {
+    "revision": "7f0472ef90ffe72f3d735c2cb103cc94",
+    "url": "/static/media/instagram.7f0472ef.svg"
+  },
+  {
+    "revision": "564af0e5b1df60c6599089e2196cab21",
+    "url": "/static/media/restart.564af0e5.jpeg"
+  },
+  {
+    "revision": "1d1370a1cb5648a82a85aa5fbf14d9ad",
+    "url": "/static/media/speaker.1d1370a1.png"
+  },
+  {
+    "revision": "24ceed931da1b39170a3c54afb5669f2",
+    "url": "/static/media/telegram.24ceed93.svg"
+  },
+  {
+    "revision": "ecebdacf82a1a62630c1dee2991ee6a9",
+    "url": "/static/media/wikipedia.ecebdacf.svg"
+  },
+  {
+    "revision": "0a835758d9f2fa9a1e63f0d034868bd3",
+    "url": "/static/media/wordpressdotcom.0a835758.png"
+  }
+]);
